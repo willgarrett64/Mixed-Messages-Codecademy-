cfe163453ad938ccb0cc8d3fe526fde5bf4ac911
Will Garrett <willgarrett64@yahoo.com>
2020-11-10 00:11:57 +0000

    Add funtion to select random position array

--- transferAdvice.js
+++ transferAdvice.js
@@ -3,14 +3,14 @@ const rawData = require('./playerData.json');
 
 
 // Initialise seperate arrays for each position to better sort the player data.
-const gkpArray = [];
-const defArray = [];
-const midArray = [];
-const fwdArray = [];
+const GKP = [];
+const DEF = [];
+const MID = [];
+const FWD = [];
 
-
+// Filter out lots of unnecesary proporties since they aren't needed for this program. Sort the data into each seperate array by checking position of each player.
 rawData.forEach(player => {
-    // Filter out lots of unnecesary proporties since they aren't needed for this program.
+    // Filter properties.
     const playerObj = {
         name: player.name,
         team: player.team,
@@ -19,23 +19,50 @@ rawData.forEach(player => {
         tsb: player.selected_by_percent,
     };
 
-    // Sort the data into each seperate array by checking position of each player. 
+    //  Sort into relevant arrays.
     switch (player.position) {
         case 'GKP':
-            gkpArray.push(playerObj);
+            GKP.push(playerObj);
             break;
     
         case 'DEF':
-            defArray.push(playerObj);            
+            DEF.push(playerObj);            
             break;
         
         case 'MID':
-            midArray.push(playerObj);            
+            MID.push(playerObj);            
             break;
         
         case 'FWD':
-            fwdArray.push(playerObj);            
+            FWD.push(playerObj);            
             break;
     };
 });
 
+
+// Function to select a random position should the 
+function selectRandomPosition() {
+    const ranNum = Math.floor(Math.random() * 4);
+    switch (ranNum) {
+        case 0:
+            return GKP;
+            break;
+        
+        case 1:
+            return DEF;
+            break;
+        
+        case 2:
+            return MID;
+            break;
+        
+        case 3:
+            return FWD;
+            break;
+    };
+}
+
+
+function selectRandomPlayer(posArr = selectRandomPosition()) {
+    
+}
